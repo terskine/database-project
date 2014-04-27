d9296763f6d6d0cd7d7815cc7015d3509d23aa3b
Student <Student@192.168.2.46>
2014-04-27 12:44:07 -0400

    Registration complete. Makes sure passwords match, userID is unique and all other fields that are required are filled in.

--- htdocs/register.php
+++ htdocs/register.php
@@ -1,12 +1,18 @@
+<?php session_start(); 
+if ($_SESSION["success"] == "false") {
+    echo 'UserID exists! Please return home to login or choose a different UserID<br>';
+    session_destroy();
+    
+}
+    ?>
 
-// Validation to be done: Data in required fields, unique fields are unique
 <form id="registration" action="submitRegistration.php" method="post">
-	Full Name: 		<input type="text" name="name" maxlength="20"><br>
-	Address:		<input type="text" name="address" maxlength="30"><br>
-	Home Phone: 	<input type="text" name="homePhone" maxlength="12"><br>
-	Mobile Phone: 	<input type="text" name="mobilePhone" maxlength="12"><br>
-	Email: 			<input type="text" name="email" maxlength="25"><br>
-	Country: 		<select name="country">
+	*Full Name: 		<input type="text" name="name" maxlength="20"><br>
+	*Address:		<input type="text" name="address" maxlength="30"><br>
+	*Home Phone: 	<input type="text" name="homePhone" maxlength="12"><br>
+	*Mobile Phone: 	<input type="text" name="mobilePhone" maxlength="12"><br>
+	*Email: 			<input type="text" name="email" maxlength="25"><br>
+	*Country: 		<select name="country">
 						<?php 
 						$conn = oci_connect('SYSTEM', 'password', '//localhost/project');
 						$stid=oci_parse($conn, "SELECT * FROM COUNTRY");
@@ -17,19 +23,28 @@
 						oci_close($conn);
 						?>
 					</select><br>
-	UserID: 			<input type="text" name="userID" maxlength="15"><br>
-	Password: 			<input type="password" name="password" id="password" maxlength="15"><br>
-	Confirm Password: 	<input type="password" name="confirmPassword" id="confirmPassword" maxlength="15"><br>
+	*UserID: 			<input type="text" name="userID" maxlength="15"><br>
+	*Password: 			<input type="password" name="password" id="password" maxlength="15"><br>
+	*Confirm Password: 	<input type="password" name="confirmPassword" id="confirmPassword" maxlength="15"><br>
 						<span name="passwordMessage" id="passwordMessage"></span><br>
 						<INPUT TYPE="button" NAME="button" Value="Submit" onClick="submitRegistration()">
-		
+                                                <span> * Indicates a required field </span>	
 	<script>
 		function submitRegistration() {
+                    requiredFields = ["name", "address", "homePhone", "mobilePhone", "email", "country", "userID"]
 			if (document.getElementById("password").value != document.getElementById("confirmPassword").value)  {
 				document.getElementById("passwordMessage").innerHTML = "Passwords do not match!";
 			} else if (document.getElementById("password").value == "") {
 				document.getElementById("passwordMessage").innerHTML = "Please enter a password!";
 			} else {
+                            
+                            for (field in requiredFields) {
+                                if (document.forms["registration"].elements[field].value == "") {
+                                    alert("Please complete all required fields!");
+                                    return;
+                                }
+                             
+                            }
 				document.getElementById("registration").submit();
 			}
 		}
--- htdocs/submitRegistration.php
+++ htdocs/submitRegistration.php
@@ -1,18 +1,31 @@
 <?php
-	
-	//$conn = oci_connect('SYSTEM', 'password', '//localhost/project');
+	session_start();
+	$conn = oci_connect('SYSTEM', 'password', '//localhost/project');
+        
+        $query = "SELECT USERID FROM CUSTOMER WHERE USERID ='".$_POST["userID"]."'";
+        $stid=oci_parse($conn, $query);
+	$result = oci_execute($stid);
+        if (oci_fetch_array($stid)) {
+            $_SESSION["success"] = "false";
+            header('Location: register.php');
+            exit;
+        }
 
 	$query = 	"INSERT INTO CUSTOMER VALUES(	CUSTOMER_PK.NEXTVAL,'"
-												.$_POST["userID"]."', 
-												'".$_POST["password"]."',
-												'".$_POST["name"]."',
-												'".$_POST["address"]."',
-												'".$_POST["homePhone"]."',
-												'".$_POST["mobilePhone"]."',
-												'".$_POST["email"]."',
-												'S',
-												".$_POST["country"]."
-											)";
-	echo $query;		
-
-?>
+                        .$_POST["userID"]."', 
+                        '".$_POST["password"]."',
+                        '".$_POST["name"]."',
+                        '".$_POST["address"]."',
+                        '".$_POST["homePhone"]."',
+                        '".$_POST["mobilePhone"]."',
+                        '".$_POST["email"]."',
+                        'S',
+                        ".$_POST["country"].")";
+	echo $query;	
+        $stid=oci_parse($conn, $query);
+	$result = oci_execute($stid);
+        
+        echo $result;
+        oci_close($conn);	
+        header('Location: registrationSuccess.html');
+	exit;
