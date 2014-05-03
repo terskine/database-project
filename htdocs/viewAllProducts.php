8139295b21a0fd3f2415e576dcde563c0f426f08
Delpheno <Delpheno@gmail.com>
2014-05-03 16:20:31 -0400

    Organized all products to a list.

--- htdocs/viewAllProducts.php
+++ htdocs/viewAllProducts.php
@@ -5,8 +5,14 @@ include 'header.php';
 
 $statement = getAllProducts();
 
+echo '<table style="width:500px">';
+echo '<td><strong>'.Name .'</strong></td>';
+echo '<td><strong>'.Cost .'</strong></td>';
+echo '<td><strong>'.Stock .'</strong></td>';
+
 while($row = oci_fetch_array($statement, OCI_BOTH))
 {
+    echo '<tr>';
     $pid = $row[1];
     $p_name = $row[3];
     $p_cost = $row[4];
@@ -15,8 +21,14 @@ while($row = oci_fetch_array($statement, OCI_BOTH))
     
     $link = '<a href="product.php?product_id='.$pid.'">'.$p_name.'</a>';
     
-    echo "Name: ". $link . " ". $p_cost. " ". $p_stock. "</br></br>";
+    echo '<td>'.$link .'</td>';
+    echo '<td>'.$p_cost .'</td>';
+    echo '<td>'.$p_stock .'</td>';
+    
+    echo '</tr>';
 }
+
+echo "</table>";
 
 include 'footer.php';
 
